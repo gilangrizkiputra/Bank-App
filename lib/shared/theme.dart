09053e4168fb4fa832c5948c9b537c427226c8b8
gilangrizkiputra <gilangrizkiptra@gmail.com>
2025-02-17 12:14:17 +0700

    feat: set up project and add assets

---
+++ lib/shared/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = Color(0xFFFFFFFF);
+Color blackColor = Color(0xFF14193F);
+Color greyColor = Color(0xFFA4A8AE);
+Color lightBackgroundColor = Color(0xFFF6F8FB);
+Color darkBackgroundColor = Color(0xFF020518);
+Color blueColor = Color(0xFF53C1F9);
+Color purpleColor = Color(0xFF5142E6);
+Color greenColor = Color(0xFF22B07D);
+Color numberBackgroundColor = Color(0xFF1A1D2E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(color: blackColor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whiteColor);
+TextStyle greyTextStyle = GoogleFonts.poppins(color: greyColor);
+TextStyle blueTextStyle = GoogleFonts.poppins(color: blueColor);
+TextStyle greenTextStyle = GoogleFonts.poppins(color: greenColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
